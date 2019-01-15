df396a4d272805d4d8ee38fed1979872ec22e7cd
andrew962 <abadia962@gmail.com>
2019-01-15 09:35:32 -0500

    Agregando native-base

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Vue-React
 Haciendo union y viendo que tal sale todo, lo bueno empieza ahora
+
+# Primeros pasos
+
+## 1
+Para poder armar un proyecto de vue-native con react-native se deben segir los pasos que nos dice la pagina oficial [Vue Native](https://vue-native.io/docs/installation.html), pero esto nos acarrea un pequeño problema con la version que nos instala 0.55.4, por eso debemos crear un proyecto totalmente nuevo react-native init <NombreDelProyecto>, este proyecto se nos instalara con la version mas actual de react-native recomiendo hacer en una carpeta totalmente aparte para que el proyecto se pueda nombrar igual al proyecto con la version 0.55.4, por que si no lo hacemos a la hora de reemplazar nos dara error.
+
+## 2
+En el proyecto que creamos con react puro debemos copiar la carpeta de android que se nos crea y pegarla en nuestro proyecto que creamos con react y vue (por eso es el tema de nombrar los proyectos iguales) reemplazamos y listo
---
+++ nuevo/App.vue
@@ -0,0 +1,54 @@
+<template>
+<nb-container>
+    <nb-header>
+      <nb-left>
+        <nb-button transparent>
+          <nb-icon :name="menu" />
+        </nb-button>
+      </nb-left>
+      <nb-body>
+        <nb-title>Header</nb-title>
+      </nb-body>
+      <nb-right />
+    </nb-header>
+    <nb-content padder>
+      <nb-text>{{mensg}}</nb-text>
+      <nb-item>
+        <nb-input v-model="mensg" />
+      </nb-item>
+    </nb-content>
+    <nb-footer>
+      <nb-footer-tab>
+        <nb-button active full>
+          <nb-text>Footer</nb-text>
+        </nb-button>
+      </nb-footer-tab>
+    </nb-footer>
+  </nb-container>
+</template>
+
+<script>
+import Vue from "vue-native-core";
+import { VueNativeBase } from "native-base";
+
+Vue.use(VueNativeBase)
+
+export default {
+  data:function(){
+    return{
+      mensg:''
+    }
+  }
+}
+</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
