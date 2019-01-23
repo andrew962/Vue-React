e2f836db1804b8512d90ec092c94c5eadc5954c8
andrew962 <abadia962@gmail.com>
2019-01-23 09:19:30 -0500

    Tablas 3,4,5

--- nuevo/App.vue
+++ nuevo/App.vue
@@ -10,6 +10,9 @@ import { StackNavigator } from 'vue-native-router'
 import App from './src/screens/App.vue'
 import Tabla1 from './src/screens/Tabla1.vue'
 import Tabla2 from './src/screens/Tabla2.vue'
+import Tabla3 from './src/screens/Tabla3.vue'
+import Tabla4 from './src/screens/Tabla4.vue'
+import Tabla5 from './src/screens/Tabla5.vue'
 
 Vue.use(VueNativeBase)
 
@@ -18,7 +21,10 @@ const AppNavigation = StackNavigator(
   {
     App: { screen: App},
     Tabla1: { screen:Tabla1 },
-    Tabla2: { screen:Tabla2 }
+    Tabla2: { screen:Tabla2 },
+    Tabla3: { screen:Tabla3 },
+    Tabla4: { screen:Tabla4 },
+    Tabla5: { screen:Tabla5 },
   },
   {
     initialRouteName: 'App',
